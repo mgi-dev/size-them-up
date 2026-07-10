c767454f5fb78b9317cb43be3cbdd370873fb652
Maxime Girma <maxime.girma@hotmail.fr>
2026-07-10 17:48:37 +0200

    initial commit, working poc

---
+++ scenes/resizable_v_2.gd
@@ -0,0 +1,106 @@
+extends RigidBody2D
+
+@onready var sprite: Polygon2D = $Polygon2D
+@onready var hitbox: CollisionShape2D = $CollisionShape2D
+@onready var mouse_detector: CollisionShape2D = $MouseDedector/MouseDetectorCollisionShape
+
+
+
+	
+	
+var RESIZE_SPEED = 0.01
+
+"""
+scaling du sprirte correcteùment instancié, 
+dernier bloc posé controle tou les autres.
+"""
+
+var current_scale: float = 1.0
+var new_scale : float = 1.0
+var current_sprite_scale : float = 1.0
+
+var selected : bool = false
+
+var _default_size: Vector2
+var _default_mouse_detector_size: Vector2
+var locked = false
+
+func _ready() -> void:
+	# dedup allow to resizeonly one box. collision shapes are shared are shared ressources by default.
+	hitbox.shape = hitbox.shape.duplicate(true)
+	var shape := hitbox.shape
+	# storing size allow to start with various size for component. else will default to (1, 1)
+	_default_size = shape.size
+	
+	mouse_detector.shape = mouse_detector.shape.duplicate(true)
+	var shape_2 := mouse_detector.shape
+	_default_mouse_detector_size = shape_2.size
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	# called every now and then, for display, animations, etc.
+	
+	if abs(current_sprite_scale - new_scale) >= RESIZE_SPEED:
+		process_sprite_resize()
+		
+		
+func _physics_process(delta: float) -> void:
+	# called 60 times per second. do not lag this.
+	if Input.is_action_pressed("size_up"):
+		
+		if locked:
+			return
+		if selected:
+			print(position.x, position.y)
+			size_up()
+			process_hitbox_resize()
+			process_mouse_detector_resize()
+			print("sprite : ", sprite.scale, new_scale)
+			print("hitbox : ", hitbox.shape.size)
+			print("position sprite : ", sprite.position)
+			print("position hitbox : ", hitbox.position)
+			# locked = true
+		
+	elif Input.is_action_pressed("size_down"):
+		size_down()
+		process_hitbox_resize()
+		process_mouse_detector_resize()
+
+	
+func size_up() -> void:
+	if selected:
+		new_scale = current_scale + RESIZE_SPEED
+	
+	
+func size_down() -> void:
+	if selected:
+		new_scale = current_scale - RESIZE_SPEED
+	
+	
+func process_hitbox_resize() -> void:
+	var shape := hitbox.shape
+	shape.size = _default_size * new_scale
+	current_scale = new_scale
+
+func process_mouse_detector_resize() -> void:
+	var shape := mouse_detector.shape
+	shape.size = _default_mouse_detector_size * new_scale
+
+
+func process_sprite_resize() -> void:
+	sprite.scale = Vector2.ONE * new_scale
+	#sprite.position.x = sprite.position.x * new_scale
+	
+func process_rigid_body_resize() -> void:
+	scale = _default_size * new_scale
+
+
+func _on_mouse_dedector_mouse_entered():
+	selected = true
+	print("true")
+
+
+func _on_mouse_dedector_mouse_exited():
+	selected = false
+	print("false")
