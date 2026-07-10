0589bd0fded56b429b135e47771f4b44ed77cdb4
Maxime Girma <maxime.girma@hotmail.fr>
2026-07-10 21:23:44 +0200

    resizable from parent

--- scenes/resizable_v_2.gd
+++ scenes/resizable_v_2.gd
@@ -1,41 +1,48 @@
 extends RigidBody2D
 
-@onready var sprite: Polygon2D = $Polygon2D
-@onready var hitbox: CollisionShape2D = $CollisionShape2D
-@onready var mouse_detector: CollisionShape2D = $MouseDedector/MouseDetectorCollisionShape
+@export var sprite: Polygon2D
+@export var hitbox: CollisionShape2D
+@export var mouse_detector: CollisionShape2D
 
 
+var RESIZE_SPEED: float = 0.01
 
-	
-	
-var RESIZE_SPEED = 0.01
+var initial_scale_transform: Vector2 # (2, 2) 
+var initial_size: Vector2 # (56, 56) 
 
-var current_scale: float = 1.0
-var new_scale : float = 1.0
-var current_sprite_scale : float = 1.0
+var current_scale_muliplier: float = 1.0
+var new_scale_multiplier : float = 1.0
+# UI is seperated from physics, need it's own var for now.
+var current_sprite_scale_multiplier : float = 1.0
 
 var selected : bool = false
 
-var _default_size: Vector2
-var _default_mouse_detector_size: Vector2
 
 func _ready() -> void:
 	# dedup allow to resizeonly one box. collision shapes are shared are shared ressources by default.
 	hitbox.shape = hitbox.shape.duplicate(true)
-	var shape := hitbox.shape
-	# storing size allow to start with various size for component. else will default to (1, 1)
-	_default_size = shape.size
-	
 	mouse_detector.shape = mouse_detector.shape.duplicate(true)
-	var shape_2 := mouse_detector.shape
-	_default_mouse_detector_size = shape_2.size
+	
+	# storing size allow to start with various size for component. else will default to (1, 1)
+	initial_size = hitbox.shape.size * scale
+	
+	# setting size for hitbox based on parent transfom scale (1.5, 1.5)
+	hitbox.shape.size = initial_size
+	mouse_detector.shape.size = initial_size
+	
+	initial_scale_transform = scale
+	sprite.scale = initial_scale_transform
+
+
+func debug():
+	print(position, sprite.position, sprite.scale, hitbox.position, hitbox.shape.size)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	# called every now and then, for display, animations, etc.
-	
-	if abs(current_sprite_scale - new_scale) >= RESIZE_SPEED:
+	# print(RESIZE_SPEED)
+	if abs(current_sprite_scale_multiplier - new_scale_multiplier) >= RESIZE_SPEED:
 		process_sprite_resize()
 		
 		
@@ -45,7 +52,9 @@ func _physics_process(delta: float) -> void:
 		size_up()
 		process_hitbox_resize()
 		process_mouse_detector_resize()
-
+		if selected:
+			# debug()
+			pass
 		
 	elif Input.is_action_pressed("size_down"):
 		size_down()
@@ -61,31 +70,34 @@ func can_size_down() -> bool:
 
 func size_up() -> void:
 	if can_size_up():
-		new_scale = current_scale + RESIZE_SPEED
-		SignalBus.resized.emit(new_scale - current_scale)
+		new_scale_multiplier = current_scale_muliplier + RESIZE_SPEED
+		SignalBus.resized.emit(new_scale_multiplier - current_scale_muliplier)
 	
 func size_down() -> void:
 	if can_size_down():
-		new_scale = current_scale - RESIZE_SPEED
-		SignalBus.resized.emit(new_scale - current_scale)
+		new_scale_multiplier = current_scale_muliplier - RESIZE_SPEED
+		SignalBus.resized.emit(new_scale_multiplier - current_scale_muliplier)
 	
 	
 func process_hitbox_resize() -> void:
-	var shape := hitbox.shape
-	shape.size = _default_size * new_scale
-	current_scale = new_scale
+	hitbox.shape.size = initial_size * new_scale_multiplier
+	current_scale_muliplier = new_scale_multiplier
+
 
 func process_mouse_detector_resize() -> void:
-	var shape := mouse_detector.shape
-	shape.size = _default_mouse_detector_size * new_scale
+	mouse_detector.shape.size = initial_size * new_scale_multiplier
 
 
 func process_sprite_resize() -> void:
-	sprite.scale = Vector2.ONE * new_scale
+	sprite.scale = initial_scale_transform * new_scale_multiplier
+	current_sprite_scale_multiplier = new_scale_multiplier
+	
 	
 func _on_mouse_dedector_mouse_entered():
+	print("entered")
 	selected = true
 
 
 func _on_mouse_dedector_mouse_exited():
+	print("pas entered")
 	selected = false
