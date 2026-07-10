f1a10c3b0ff9d2f13911e42138fe4c478c68ece8
Maxime Girma <maxime.girma@hotmail.fr>
2026-07-10 22:01:39 +0200

    more versatiles resize modes

--- scenes/resizable_v_2.gd
+++ scenes/resizable_v_2.gd
@@ -5,6 +5,10 @@ extends RigidBody2D
 @export var mouse_detector: CollisionShape2D
 
 
+enum RESIZE_MODE {ALL, VERTICAL, HORIZONTAL}
+
+@export var resize_mode: RESIZE_MODE = RESIZE_MODE.ALL
+
 var RESIZE_SPEED: float = 0.01
 
 var initial_scale_transform: Vector2 # (2, 2) 
@@ -19,20 +23,14 @@ var selected : bool = false
 
 
 func _ready() -> void:
-	# dedup allow to resizeonly one box. collision shapes are shared are shared ressources by default.
-	hitbox.shape = hitbox.shape.duplicate(true)
-	mouse_detector.shape = mouse_detector.shape.duplicate(true)
-	
 	# storing size allow to start with various size for component. else will default to (1, 1)
 	initial_size = hitbox.shape.size * scale
 	
 	# setting size for hitbox based on parent transfom scale (1.5, 1.5)
-	hitbox.shape.size = initial_size
-	mouse_detector.shape.size = initial_size
-	
 	initial_scale_transform = scale
 	sprite.scale = initial_scale_transform
-
+	hitbox.scale = initial_scale_transform
+	mouse_detector.scale = initial_scale_transform
 
 func debug():
 	print(position, sprite.position, sprite.scale, hitbox.position, hitbox.shape.size)
@@ -49,17 +47,19 @@ func _process(delta):
 func _physics_process(delta: float) -> void:
 	# called 60 times per second. do not lag this.
 	if Input.is_action_pressed("size_up"):	
-		size_up()
-		process_hitbox_resize()
-		process_mouse_detector_resize()
+		if can_size_up():
+			size_up()
+			process_hitbox_resize()
+			process_mouse_detector_resize()
 		if selected:
 			# debug()
 			pass
 		
 	elif Input.is_action_pressed("size_down"):
-		size_down()
-		process_hitbox_resize()
-		process_mouse_detector_resize()
+		if can_size_down():
+			size_down()
+			process_hitbox_resize()
+			process_mouse_detector_resize()
 
 func can_size_up() -> bool:
 	return selected and GameState.can_size_up()
@@ -69,27 +69,39 @@ func can_size_down() -> bool:
 	return selected and GameState.can_size_down()
 
 func size_up() -> void:
-	if can_size_up():
-		new_scale_multiplier = current_scale_muliplier + RESIZE_SPEED
-		SignalBus.resized.emit(new_scale_multiplier - current_scale_muliplier)
+	new_scale_multiplier = current_scale_muliplier + RESIZE_SPEED
+	SignalBus.resized.emit(new_scale_multiplier - current_scale_muliplier)
 	
 func size_down() -> void:
-	if can_size_down():
-		new_scale_multiplier = current_scale_muliplier - RESIZE_SPEED
-		SignalBus.resized.emit(new_scale_multiplier - current_scale_muliplier)
-	
+	new_scale_multiplier = current_scale_muliplier - RESIZE_SPEED
+	SignalBus.resized.emit(new_scale_multiplier - current_scale_muliplier)
+
+
+
+func get_transformed_scale(_scale: Vector2, transform: float) -> Vector2:
+	# Transform the given Vector depending on selected resize mode.
+	if resize_mode == RESIZE_MODE.ALL:
+		return _scale * transform
+	if resize_mode == RESIZE_MODE.HORIZONTAL:
+		return Vector2(_scale.x * transform, _scale.y)
+	if resize_mode == RESIZE_MODE.VERTICAL:
+		return Vector2(_scale.x, _scale.y * transform)
+	else: 
+		return _scale * transform
+
+
 	
 func process_hitbox_resize() -> void:
-	hitbox.shape.size = initial_size * new_scale_multiplier
+	hitbox.scale = get_transformed_scale(initial_scale_transform, new_scale_multiplier)
 	current_scale_muliplier = new_scale_multiplier
 
 
 func process_mouse_detector_resize() -> void:
-	mouse_detector.shape.size = initial_size * new_scale_multiplier
+	mouse_detector.scale = get_transformed_scale(initial_scale_transform, new_scale_multiplier)
 
 
 func process_sprite_resize() -> void:
-	sprite.scale = initial_scale_transform * new_scale_multiplier
+	sprite.scale = get_transformed_scale(initial_scale_transform, new_scale_multiplier)
 	current_sprite_scale_multiplier = new_scale_multiplier
 	
 	
