d398ebcb6deebc30d8875f040ec201cfc953768e
Maxime Girma <maxime.girma@hotmail.fr>
2026-07-22 11:25:52 +0200

    Add unfinished intro scene

--- scenes/landscape/computer/computer.gd
+++ scenes/landscape/computer/computer.gd
@@ -1,9 +1,14 @@
 extends Node2D
 
 
+@export var enable: bool = true
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	if not enable:
+		$Flashing.is_flashing = false
+		$Interractable.already_interacted = true
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -11,8 +16,9 @@ func _process(delta):
 	pass
 
 func on_interact():
-	$Flashing.is_flashing = false
-	var parent = get_parent()
-	if parent:
-		if parent.has_method("on_interact"):
-			parent.on_interact(self)
+	if enable:
+		$Flashing.is_flashing = false
+		var parent = get_parent()
+		if parent:
+			if parent.has_method("on_interact"):
+				parent.on_interact(self)
