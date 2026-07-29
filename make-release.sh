73ff6a949ba9394cb393c81cce46729403a0e68f
Maxime Girma <maxime.girma@hotmail.fr>
2026-07-29 19:21:53 +0200

    Add the cheapest working CI-CD pipeline

---
+++ make-release.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+
+set -e
+
+source .env
+version=$(grep -o '"build":[[:space:]]*[0-9]*' settings.json | grep -o '[0-9]*')
+version=$((version + 1))
+sed -i "s/\"build\":[[:space:]]*[0-9]*/\"build\": $version/" settings.json
+
+
+
+PROJECT_DIR="$(pwd)"
+OUTPUT_DIR="$PROJECT_DIR/build/web/$version"
+
+
+mkdir -p "$OUTPUT_DIR"
+
+"$GODOT_PATH" \
+    --headless \
+    --path "$PROJECT_DIR" \
+    --export-release "Web" \
+    "$OUTPUT_DIR/index.html"
+
+echo "Build Done."
+
+
+
+
+########################### ZIP IT
+
+
+
+#SOURCE_DIR="$(pwd)/build/web"
+OUTPUT_ZIP="$(pwd)/build/web/$version/web.zip"
+
+SEVENZIP="C:\Program Files\7-Zip\7z.exe"
+command -v 7z >/dev/null 2>&1 && SEVENZIP="7z"
+
+rm -f "$OUTPUT_ZIP"
+
+pushd "$OUTPUT_DIR" >/dev/null
+"$SEVENZIP" a -tzip "$OUTPUT_ZIP" .
+popd >/dev/null
+
+echo "Archive created: $OUTPUT_ZIP"
+
+
+
+
+######################## UPLOAD ON ITCH
+
+
+BUILD_DIR="build/web/$version"
+CHANNEL="web"
+
+
+export BUTLER_API_KEY
+
+echo "Uploading to itch.io..."
+
+
+$BUTLER_PATH push \
+    "$BUILD_DIR" \
+    "$ITCH_USER/$ITCH_GAME:$CHANNEL"
+
+echo "Upload complete!"
+
+$BUTLER_PATH status $ITCH_USER/$ITCH_GAME:$CHANNEL
+
--- scenes/ui/debug_display/scene_info.gd
+++ scenes/ui/debug_display/scene_info.gd
@@ -4,11 +4,18 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	info_text.text = get_tree().current_scene.name 
+	info_text.text += " | god mode : "
+	info_text.text += "on" if GameState.god_mode else "off" 
+	info_text.text += " | build: " + get_build_number() 
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	info_text.text = get_tree().current_scene.name 
-	info_text.text += " | god mode : "
-	info_text.text += "on" if GameState.god_mode else "off" 
+	pass
+
+
+func get_build_number() -> String: 
+	var file = FileAccess.open("res://settings.json", FileAccess.READ)
+	var settings = JSON.parse_string(file.get_as_text())
+	return str(settings.build)
